724cca86da4a4807b2b2774ad0941ed99fa84c2b
jrjrspnl <jerwinespenile@gmail.com>
2025-05-25 10:59:10 +0800

    first commit

---
+++ Four-end-coders/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Test from "./components/test.jsx";
+
+function App() {
+  return (
+    <>
+      <Test />
+    </>
+  );
+}
+
+export default App;
---
+++ Four-end-coders/src/components/test.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const test = () => {
+  return <div>test tstest</div>;
+};
+
+export default test;

c48d79026f3a79d070e95a11289c31de3f2cad71
jrjrspnl <jerwinespenile@gmail.com>
2025-05-26 14:15:13 +0800

    Modified App.jsx

--- Four-end-coders/src/App.jsx
+++ Four-end-coders/src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import Footer from "./components/Footer.jsx";
 import Hero from "./components/Hero.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Hero />
       <Footer />
     </>

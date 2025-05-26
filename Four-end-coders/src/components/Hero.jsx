d52355a0d8aa6f0cec9ee7f74f194a3dcd3142ce
jrjrspnl <jerwinespenile@gmail.com>
2025-05-26 18:10:26 +0800

    Added a text in hero component

--- Four-end-coders/src/components/Hero.jsx
+++ Four-end-coders/src/components/Hero.jsx
@@ -3,10 +3,26 @@ import heroBg from "../assets/images/thebeatles.jpg";
 const Hero = () => {
   return (
     <>
-      <div className="w-full h-screen relative">
+      <div className="w-full h-screen relative text-white">
         <img src={heroBg} alt="" className="w-full h-full object-cover " />
-        <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black/100"></div>
-        <div className="absolute w-full h-full inset-0 bg-gray-900/30"></div>
+        <div className="absolute w-full h-full inset-0 bg-gray-900/30">
+          <div className="flex items-end absolute inset-0 bg-gradient-to-b from-transparent to-black/100 ">
+            <div className="w-full max-w-7xl mx-auto p-4">
+              <h1 className="text-4xl font-bold">Interview with the Beatles</h1>
+              <div className="mt-4 max-w-md">
+                <p className="text-medium">
+                  1-on-1 interview with the legendary band and an inside look at
+                  their journey through out the years.{" "}
+                </p>
+                <div className="mt-8 mb-15">
+                  <button className="w-full sm:w-[150px] bg-gradient-to-r from-[#C22026] to-[#C400CB] rounded-3xl py-2">
+                    Read more
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );

64f5993a99c47c3a5c92e6ecf5c2a966d40f96ca
jrjrspnl <jerwinespenile@gmail.com>
2025-05-26 15:19:23 +0800

    Added styles for navbar component

--- Four-end-coders/src/components/Navbar.jsx
+++ Four-end-coders/src/components/Navbar.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import heroLogo from "../assets/logo/thebeatleslogo.png";
+import ticketLogo from "../assets/logo/ticketlogo.png";
+import hamburger from "../assets/logo/hamburger.png";
+import userlogo from "../assets/logo/userlogo.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <div className="w-full flex flex-row justify-between bg-red-500 items-center text-white px-10 py-5">
+        <img src={heroLogo} alt={heroLogo} className="w-[70px]" />
+        <div className="flex items-center ">
+          {" "}
+          <img src={ticketLogo} alt={ticketLogo} className="mr-5" />
+          <p className="text-md font-medium mr-10">Events</p>
+          <div className="flex gap-x-2 bg-[#1E1F22] p-2 rounded-3xl">
+            <img src={hamburger} alt={hamburger} className="p-2" />
+            <img
+              src={userlogo}
+              alt={userlogo}
+              className="bg-[#101010] rounded-2xl p-2"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;

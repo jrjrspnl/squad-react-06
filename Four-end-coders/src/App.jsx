c4c4b3b0680dd7b6c493bc982e1ccd5fca481d16
Franklin <fabonero@ssct.edu.ph>
2025-05-26 12:46:28 +0800

    Resolved merge conflicts in vite.config.js and App.jsx

--- Four-end-coders/src/App.jsx
+++ Four-end-coders/src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import Footer from "./components/Footer.jsx";
 
 function App() {
@@ -8,11 +7,6 @@ function App() {
       <Footer />
     </>
   );
-=======
-
-function App() {
-  return <></>;
->>>>>>> bb1d2b0179ec07b44921cc9a8e0e93960f3cdff9
 }
 
 export default App;

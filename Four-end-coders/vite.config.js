15ab4f2a99589266d4533cddf1d47152a726ecd7
Franklin <fabonero@ssct.edu.ph>
2025-05-26 12:45:20 +0800

    Resolved merge conflicts in vite.config.js and App.jsx

--- Four-end-coders/vite.config.js
+++ Four-end-coders/vite.config.js
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
-
-=======
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
->>>>>>> bb1d2b0179ec07b44921cc9a8e0e93960f3cdff9
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-<<<<<<< HEAD
-})
-=======
 });
->>>>>>> bb1d2b0179ec07b44921cc9a8e0e93960f3cdff9

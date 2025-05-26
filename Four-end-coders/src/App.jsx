f5a5928db795317c666d175e909a07bb301fb605
jrjrspnl <jerwinespenile@gmail.com>
2025-05-26 13:12:11 +0800

    Add hero component in app.jsx

--- Four-end-coders/src/App.jsx
+++ Four-end-coders/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Footer from "./components/footer/Footer.jsx";
+import Hero from "./components/hero/Hero.jsx";
 
 function App() {
   return (
     <>
       <Footer />
+      <Hero />
     </>
   );
 }

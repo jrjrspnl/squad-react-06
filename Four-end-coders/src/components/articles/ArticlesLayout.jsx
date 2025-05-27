e533fedbe1cc51dcdff2a109ee3ab0169c704bd1
DESKTOP-AA1S8CS\SCAMMER <giesongacho1@gmail.com>
2025-05-28 00:36:40 +0800

    starting layouting article (not yet finish)

--- Four-end-coders/src/components/articles/ArticlesLayout.jsx
+++ Four-end-coders/src/components/articles/ArticlesLayout.jsx
@@ -1,19 +1,36 @@
-import React, {useEffect} from 'react'
-import {getEvents} from '../../sevices/events.service.js'
+import React, { useEffect, useState } from "react";
+import { getEvents } from "../../sevices/events.service.js";
 
-const ArticlesLayout = () => {
-  const articleList = async() => {
-    const data = await getEvents();
-    console.log('datas',data)
-  }
-  useEffect(() => {
-    articleList()
-  }, [])
+const ArticleCard = ({ article }) => {
   return (
     <div>
-        
+      <img alt="img1" src={article.imageUrl} />
+      <span>{article.title}</span>
+      <span>{article.description}</span>
+      <span>{article.published}</span>
     </div>
-  )
-}
+  );
+};
+const ArticlesLayout = () => {
+  const [articleList, setArticleList] = useState([]);
+  // const [category, setCategory] = useState([]);
+  // const [selectedCategory, setSelectedCategory] = useState(null);
+  const articleData = async () => {
+    const data = await getEvents();
+    const broadway = data.find((data) => data.category === "broadway");
+    console.log("broadway", broadway);
+    setArticleList(data);
+  };
+  useEffect(() => {
+    articleData();
+  }, []);
+  return (
+    <>
+      {articleList.map((article) => (
+        <ArticleCard key={article.id} article={article} />
+      ))}
+    </>
+  );
+};
 
-export default ArticlesLayout
+export default ArticlesLayout;

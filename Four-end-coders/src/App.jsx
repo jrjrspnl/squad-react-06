a038e06d7095fe5ffde46389964ffad0a9022ad6
Gieson Gacho <giesongacho1@gmail.com>
2025-05-30 19:54:30 +0800

    new update

--- Four-end-coders/src/App.jsx
+++ Four-end-coders/src/App.jsx
@@ -3,14 +3,14 @@ import React from "react";
 import Footer from "./components/Footer.jsx";
 import Hero from "./components/Hero.jsx";
 import Navbar from "./components/Navbar.jsx";
-import ArticlesLayout from "./components/articles/ArticlesLayout.jsx";
+import ArticleContainer from "./components/article/ArticleContainer.jsx";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <ArticlesLayout />
+      <ArticleContainer />
       <Footer />
     </>
   );

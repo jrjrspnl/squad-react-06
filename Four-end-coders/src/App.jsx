788df26265383f1f8af7fe92b7da10994a9b19f6
jrjrspnl <jerwinespenile@gmail.com>
2025-05-30 22:22:57 +0800

    Added a menu in navbar

--- Four-end-coders/src/App.jsx
+++ Four-end-coders/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import Footer from "./components/Footer.jsx";
 import Hero from "./components/Hero.jsx";
-import Navbar from "./components/Navbar.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
 import NewsPage from "./components/NewsPage/NewsPage.jsx";
 
 function App() {

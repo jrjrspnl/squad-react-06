eed8f809dd73ebdfd481a7a122e314faa6d3539e
DESKTOP-AA1S8CS\SCAMMER <giesongacho1@gmail.com>
2025-06-05 01:52:45 +0800

    arrange responsive design not final

--- Four-end-coders/src/components/article/ArticleComponent.jsx
+++ Four-end-coders/src/components/article/ArticleComponent.jsx
@@ -3,103 +3,213 @@ import { getEvents } from "../../sevices/events.service.js";
 import LeftArrow from "../../assets/icon/Group 3510.svg";
 import RightArrow from "../../assets/icon/Group 3543.svg";
 
+const checkScreen = () => {
+  const width = window.innerWidth;
+  const mobileWith = 640;
+  return width < mobileWith;
+};
+
+const mobileSize = checkScreen();
 const Broadway = ({ broadway }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const nextSlide = () => {
+    if (currentSlide < Math.ceil(broadway.length / 2) - 1) {
+      setCurrentSlide(currentSlide + 1);
+    }
+  };
+
+  const prevSlide = () => {
+    if (currentSlide > 0) {
+      setCurrentSlide(currentSlide - 1);
+    }
+  };
   return (
-    <>
-      <div className="flex flex-row items-cente justify-between m-5">
-        <h2 className="text-white">Broadway</h2>
-        <div className="flex gap-2">
-          <span>
-            <img src={LeftArrow} alt="" />
-          </span>
-          <span>
-            <img src={RightArrow} alt="" />
-          </span>
+    <div className="mt-5">
+      {!mobileSize && (
+        <div className="flex flex-row items-center justify-between">
+          <h2 className="text-white text-[24px] font-bold">Musical</h2>
+          <div className="flex gap-2">
+            <button onClick={prevSlide} disabled={currentSlide === 0}>
+              <img className="h-[48px] w-[48px]" src={LeftArrow} alt="" />
+            </button>
+            <button
+              onClick={nextSlide}
+              disabled={currentSlide >= Math.ceil(broadway.length / 2) - 1}
+            >
+              <img className="h-[48px] w-[48px]" src={RightArrow} alt="" />
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="md:flex">
+      )}
+      <div
+        className="mt-2 flex flex-col md:flex-row  overflow-x-hidden mt-5 md:gap-x-5"
+        style={{
+          transform: `translateX(-${currentSlide * 100}%)`,
+          transition: "transform 0.5s ease",
+        }}
+      >
         {broadway.map((broadway) => (
-          <div style={{ backgroundColor: "#1E1F22" }} className=" m-5 p-5 flex">
+          <div
+            style={{ backgroundColor: "#1E1F22" }}
+            className=" p-5 my-5 md:flex rounded-[16px] cursor-pointer hover:opacity-70"
+          >
             <img
               alt={broadway.title}
               src={broadway.imageUrl}
-              className="h-64 w-64"
+              className="h-[219.09px] md:w-[311px] md:h-[251px] md:w-[251px]"
             />
-            <div className="text-white flex flex-col justify-between ml-5">
-              <span className="">{broadway.title}</span>
-              <span className="">{broadway.description}</span>
-              <span className="">{broadway.published}</span>
+            <div className="text-white  flex flex-col mt-5 md:ml-[16px]">
+              <span className="md:h-[5.063rem] md:w-[16.938rem] md:text-[1.25rem] font-medium">
+                {broadway.title}
+              </span>
+              <span className="text-[14px] font-normal mt-[8px]">
+                {broadway.description}
+              </span>
+              <div className="text-[14px] font-normal flex flex-col mt-[52px]">
+                <span className="">Publish:</span>
+                <span className="">{broadway.published}</span>
+              </div>
             </div>
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 };
 const Musical = ({ musical }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const nextSlide = () => {
+    if (currentSlide < Math.ceil(musical.length / 2) - 1) {
+      setCurrentSlide(currentSlide + 1);
+    }
+  };
+
+  const prevSlide = () => {
+    if (currentSlide > 0) {
+      setCurrentSlide(currentSlide - 1);
+    }
+  };
   return (
-    <>
-      <div className="flex flex-row items-cente justify-between m-5">
-        <h2 className="text-white">Musical</h2>
-        <div className="flex gap-2">
-          <span>
-            <img src={LeftArrow} alt="" />
-          </span>
-          <span>
-            <img src={RightArrow} alt="" />
-          </span>
+    <div className="mt-5">
+      {!mobileSize && (
+        <div className="flex flex-row items-center justify-between">
+          <h2 className="text-white text-[24px] font-bold">Musical</h2>
+          <div className="flex gap-2">
+            <button onClick={prevSlide} disabled={currentSlide === 0}>
+              <img className="h-[48px] w-[48px]" src={LeftArrow} alt="" />
+            </button>
+            <button
+              onClick={nextSlide}
+              disabled={currentSlide >= Math.ceil(musical.length / 2) - 1}
+            >
+              <img className="h-[48px] w-[48px]" src={RightArrow} alt="" />
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="md:flex">
+      )}
+      <div
+        className="mt-2 flex flex-col md:flex-row  overflow-x-hidden mt-5 md:gap-x-5"
+        style={{
+          transform: `translateX(-${currentSlide * 100}%)`,
+          transition: "transform 0.5s ease",
+        }}
+      >
         {musical.map((musical) => (
-          <div style={{ backgroundColor: "#1E1F22" }} className=" m-5 p-5 flex">
+          <div
+            style={{ backgroundColor: "#1E1F22" }}
+            className=" p-5 my-5 md:flex rounded-[16px] cursor-pointer hover:opacity-70"
+          >
             <img
               alt={musical.title}
               src={musical.imageUrl}
-              className="h-64 w-64"
+              className="h-[219.09px] md:w-[311px] md:h-[251px] md:w-[251px]"
             />
-            <div className="text-white flex flex-col justify-between ml-5">
-              <span className="">{musical.title}</span>
-              <span className="">{musical.description}</span>
-              <span className="">{musical.published}</span>
+            <div className="text-white  flex flex-col mt-5 md:ml-[16px]">
+              <span className="md:h-[5.063rem] md:w-[16.938rem] md:text-[1.25rem] font-medium">
+                {musical.title}
+              </span>
+              <span className="text-[14px] font-normal mt-[8px]">
+                {musical.description}
+              </span>
+              <div className="text-[14px] font-normal flex flex-col mt-[52px]">
+                <span className="">Publish:</span>
+                <span className="">{musical.published}</span>
+              </div>
             </div>
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 };
 const Ballet = ({ ballet }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  // Add navigation functions
+  const nextSlide = () => {
+    if (currentSlide < Math.ceil(ballet.length / 2) - 1) {
+      setCurrentSlide(currentSlide + 1);
+    }
+  };
+
+  const prevSlide = () => {
+    if (currentSlide > 0) {
+      setCurrentSlide(currentSlide - 1);
+    }
+  };
   return (
-    <>
-      <div className="flex flex-row items-cente justify-between m-5">
-        <h2 className="text-white">Ballet</h2>
-        <div className="flex gap-2">
-          <span>
-            <img src={LeftArrow} alt="" />
-          </span>
-          <span>
-            <img src={RightArrow} alt="" />
-          </span>
+    <div className="mt-5">
+      {!mobileSize && (
+        <div className="flex flex-row items-center justify-between">
+          <h2 className="text-white text-[24px] font-bold">Musical</h2>
+          <div className="flex gap-2">
+            <button onClick={prevSlide} disabled={currentSlide === 0}>
+              <img className="h-[48px] w-[48px]" src={LeftArrow} alt="" />
+            </button>
+            <button
+              onClick={nextSlide}
+              disabled={currentSlide >= Math.ceil(ballet.length / 2) - 1}
+            >
+              <img className="h-[48px] w-[48px]" src={RightArrow} alt="" />
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="md:flex">
+      )}
+      <div
+        className="mt-2 flex flex-col md:flex-row  overflow-x-hidden mt-5 md:gap-x-5"
+        style={{
+          transform: `translateX(-${currentSlide * 100}%)`,
+          transition: "transform 0.5s ease",
+        }}
+      >
         {ballet.map((ballet) => (
-          <div style={{ backgroundColor: "#1E1F22" }} className=" m-5 p-5 flex">
+          <div
+            style={{ backgroundColor: "#1E1F22" }}
+            className=" p-5 my-5 md:flex rounded-[16px] cursor-pointer hover:opacity-70"
+          >
             <img
               alt={ballet.title}
               src={ballet.imageUrl}
-              className="h-64 w-64"
+              className="h-[219.09px] md:w-[311px] md:h-[251px] md:w-[251px]"
             />
-            <div className="text-white flex flex-col justify-between ml-5">
-              <span className="">{ballet.title}</span>
-              <span className="">{ballet.description}</span>
-              <span className="">{ballet.published}</span>
+            <div className="text-white  flex flex-col mt-5 md:ml-[16px]">
+              <span className="md:h-[5.063rem] md:w-[16.938rem] md:text-[1.25rem] font-medium">
+                {ballet.title}
+              </span>
+              <span className="text-[14px] font-normal mt-[8px]">
+                {ballet.description}
+              </span>
+              <div className="text-[14px] font-normal flex flex-col mt-[52px]">
+                <span className="">Publish:</span>
+                <span className="">{ballet.published}</span>
+              </div>
             </div>
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 };
 
@@ -127,39 +237,44 @@ const ArticleComponent = () => {
 
   return (
     <div style={{ backgroundColor: "#000000" }}>
-      <div>
-        <div className="flex justify-between items-center mx-5 pt-2">
-          <h2 className="text-white">Article</h2>
-          <select
-            className="text-white bg-slate-900 py-2 px-2 border-0"
-            id="cars"
-            onChange={handleCategory}
-          >
-            <option className="text-white" value="all">
-              All
-            </option>
-            <option className="text-white" value="broadway">
-              broadway
-            </option>
-            <option className="text-white" value="musical">
-              musical
-            </option>
-            <option className="text-white" value="ballet">
-              ballets
-            </option>
-          </select>
+      <div className="px-20">
+        <div>
+          <div className="flex justify-between items-center pt-5">
+            <h2 className="text-white text-[24px] md:text-[32px] font-bold ">
+              Articles
+            </h2>
+            <select
+              className="text-white bg-slate-900 w-[143px] p-[16px] border-0 h-[56px] md:w-[180px] "
+              id="cars"
+              onChange={handleCategory}
+              style={{ borderRadius: "16px" }}
+            >
+              <option className="text-white" value="all">
+                All
+              </option>
+              <option className="text-white" value="broadway">
+                broadway
+              </option>
+              <option className="text-white" value="musical">
+                musical
+              </option>
+              <option className="text-white" value="ballet">
+                ballets
+              </option>
+            </select>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col">
-        {(filter === "all" || filter === "broadway") && broadway.length > 0 && (
-          <Broadway broadway={broadway} />
-        )}
-        {(filter === "all" || filter === "musical") && musical.length > 0 && (
-          <Musical musical={musical} />
-        )}
-        {(filter === "all" || filter === "ballet") && ballet.length > 0 && (
-          <Ballet ballet={ballet} />
-        )}
+        <div className="flex flex-col">
+          {(filter === "all" || filter === "broadway") && (
+            <Broadway broadway={broadway} />
+          )}
+          {(filter === "all" || filter === "musical") && (
+            <Musical musical={musical} />
+          )}
+          {(filter === "all" || filter === "ballet") && (
+            <Ballet ballet={ballet} />
+          )}
+        </div>
       </div>
     </div>
   );

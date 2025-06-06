7d2f27f803c51e853b99a240d5a93f69d092ce9a
DESKTOP-AA1S8CS\SCAMMER <giesongacho1@gmail.com>
2025-06-06 19:50:18 +0800

    destructured components

--- Four-end-coders/src/components/article/ArticleComponent.jsx
+++ Four-end-coders/src/components/article/ArticleComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { getEvents } from "../../sevices/events.service.js";
 import LeftArrow from "../../assets/icon/Group 3510.svg";
 import RightArrow from "../../assets/icon/Group 3543.svg";
@@ -10,257 +10,229 @@ const checkScreen = () => {
 };
 
 const mobileSize = checkScreen();
-const Broadway = ({ broadway }) => {
-  const [currentSlide, setCurrentSlide] = useState(0);
 
-  const nextSlide = () => {
-    if (currentSlide < Math.ceil(broadway.length / 2) - 1) {
-      setCurrentSlide(currentSlide + 1);
-    }
-  };
+// Broadway Section
+const BroadwayCard = ({ title, imageUrl, description, published }) => (
+  <div
+    style={{ backgroundColor: "#1E1F22" }}
+    className=" p-5 my-5 md:flex rounded-[16px] cursor-pointer hover:opacity-70 overflow-hidden"
+  >
+    <img
+      alt={title}
+      src={imageUrl}
+      className="h-[219.09px] w-[311px] md:h-[251px] md:w-[251px]"
+    />
+    <div className="text-white  flex flex-col mt-5 md:ml-[16px]">
+      <span className="md:h-[5.063rem] md:w-[16.938rem] md:text-[1.25rem] font-medium">
+        {title}
+      </span>
+      <span
+        style={{ color: "#FFFFFF99" }}
+        className="text-[14px] font-normal mt-[8px]"
+      >
+        {description}
+      </span>
+      <div className="text-[14px] font-normal flex flex-col mt-[52px]">
+        <span className="">Publish:</span>
+        <span className="">{published}</span>
+      </div>
+    </div>
+  </div>
+);
 
-  const prevSlide = () => {
-    if (currentSlide > 0) {
-      setCurrentSlide(currentSlide - 1);
-    }
-  };
+const BroadwayComponent = ({ broadway, loadArticleHeader }) => {
   return (
     <div className="mt-5">
       {!mobileSize && (
         <div className="flex flex-row items-center justify-between">
-          <h2 className="text-white text-[24px] font-bold">Broaway</h2>
-          <div className="flex gap-2">
-            <button onClick={prevSlide} disabled={currentSlide === 0}>
-              <img className="h-[48px] w-[48px]" src={LeftArrow} alt="" />
-            </button>
-            <button
-              onClick={nextSlide}
-              disabled={currentSlide >= Math.ceil(broadway.length / 2) - 1}
-            >
-              <img className="h-[48px] w-[48px]" src={RightArrow} alt="" />
-            </button>
-          </div>
+          {loadArticleHeader && (
+            <>
+              <h2 className="text-white text-[24px] font-bold">Broaway</h2>
+              <div className="flex gap-2">
+                <button>
+                  <img className="h-[48px] w-[48px]" src={LeftArrow} alt="" />
+                </button>
+                <button>
+                  <img className="h-[48px] w-[48px]" src={RightArrow} alt="" />
+                </button>
+              </div>
+            </>
+          )}
         </div>
       )}
-      <div
-        className="mt-2 flex flex-col md:flex-row  overflow-x-hidden mt-5 md:gap-x-5"
-        style={{
-          transform: `translateX(-${currentSlide * 100}%)`,
-          transition: "transform 0.5s ease",
-        }}
-      >
+      <div className="mt-2 flex flex-col md:flex-row  overflow-x-hidden mt-5 md:gap-x-5 ">
         {broadway.map((broadway) => (
-          <div
-            key={broadway.id}
-            style={{ backgroundColor: "#1E1F22" }}
-            className=" p-5 my-5 md:flex rounded-[16px] cursor-pointer hover:opacity-70 overflow-hidden"
-          >
-            <img
-              alt={broadway.title}
-              src={broadway.imageUrl}
-              className="h-[219.09px] w-[311px] md:h-[251px] md:w-[251px]"
-            />
-            <div className="text-white  flex flex-col mt-5 md:ml-[16px]">
-              <span className="md:h-[5.063rem] md:w-[16.938rem] md:text-[1.25rem] font-medium">
-                {broadway.title}
-              </span>
-              <span style={{color: "#FFFFFF99"}} className="text-[14px] font-normal mt-[8px]">
-                {broadway.description}
-              </span>
-              <div className="text-[14px] font-normal flex flex-col mt-[52px]">
-                <span className="">Publish:</span>
-                <span className="">{broadway.published}</span>
-              </div>
-            </div>
-          </div>
+          <MusicalCard key={broadway.id} {...broadway} />
         ))}
       </div>
     </div>
   );
 };
-const Musical = ({ musical }) => {
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  const nextSlide = () => {
-    if (currentSlide < Math.ceil(musical.length / 2) - 1) {
-      setCurrentSlide(currentSlide + 1);
-    }
-  };
-
-  const prevSlide = () => {
-    if (currentSlide > 0) {
-      setCurrentSlide(currentSlide - 1);
-    }
-  };
+// Musical Section
+const MusicalCard = ({ title, imageUrl, description, published }) => (
+  <div
+    style={{ backgroundColor: "#1E1F22" }}
+    className=" p-5 my-5 md:flex rounded-[16px] cursor-pointer hover:opacity-70 overflow-hidden"
+  >
+    <img
+      alt={title}
+      src={imageUrl}
+      className="h-[219.09px] w-[311px] md:h-[251px] md:w-[251px]"
+    />
+    <div className="text-white  flex flex-col mt-5 md:ml-[16px]">
+      <span className="md:h-[5.063rem] md:w-[16.938rem] md:text-[1.25rem] font-medium">
+        {title}
+      </span>
+      <span
+        style={{ color: "#FFFFFF99" }}
+        className="text-[14px] font-normal mt-[8px]"
+      >
+        {description}
+      </span>
+      <div className="text-[14px] font-normal flex flex-col mt-[52px]">
+        <span className="">Publish:</span>
+        <span className="">{published}</span>
+      </div>
+    </div>
+  </div>
+);
+const MusicalComponent = ({ musical, loadArticleHeader }) => {
   return (
     <div className="mt-5">
       {!mobileSize && (
         <div className="flex flex-row items-center justify-between">
-          <h2 className="text-white text-[24px] font-bold">Musical</h2>
-          <div className="flex gap-2">
-            <button onClick={prevSlide} disabled={currentSlide === 0}>
-              <img className="h-[48px] w-[48px]" src={LeftArrow} alt="" />
-            </button>
-            <button
-              onClick={nextSlide}
-              disabled={currentSlide >= Math.ceil(musical.length / 2) - 1}
-            >
-              <img className="h-[48px] w-[48px]" src={RightArrow} alt="" />
-            </button>
-          </div>
+          {loadArticleHeader && (
+            <>
+              <h2 className="text-white text-[24px] font-bold">Musical</h2>
+              <div className="flex gap-2">
+                <button>
+                  <img className="h-[48px] w-[48px]" src={LeftArrow} alt="" />
+                </button>
+                <button>
+                  <img className="h-[48px] w-[48px]" src={RightArrow} alt="" />
+                </button>
+              </div>
+            </>
+          )}
         </div>
       )}
-      <div
-        className="mt-2 flex flex-col md:flex-row  overflow-x-hidden mt-5 md:gap-x-5"
-        style={{
-          transform: `translateX(-${currentSlide * 100}%)`,
-          transition: "transform 0.5s ease",
-        }}
-      >
+      <div className="mt-2 flex flex-col md:flex-row  overflow-x-hidden mt-5 md:gap-x-5 ">
         {musical.map((musical) => (
-          <div
-            key={musical.id}
-            style={{ backgroundColor: "#1E1F22" }}
-            className=" p-5 my-5 md:flex rounded-[16px] cursor-pointer hover:opacity-70"
-          >
-            <img
-              alt={musical.title}
-              src={musical.imageUrl}
-              className="h-[219.09px] w-[311px] md:h-[251px] md:w-[251px]"
-            />
-            <div className="text-white  flex flex-col mt-5 md:ml-[16px]">
-              <span className="md:h-[5.063rem] md:w-[16.938rem] md:text-[1.25rem] font-medium">
-                {musical.title}
-              </span>
-              <span style={{color: "#FFFFFF99"}} className="text-[14px] font-normal mt-[8px]">
-                {musical.description}
-              </span>
-              <div className="text-[14px] font-normal flex flex-col mt-[52px]">
-                <span className="">Publish:</span>
-                <span className="">{musical.published}</span>
-              </div>
-            </div>
-          </div>
+          <BroadwayCard key={musical.id} {...musical} />
         ))}
       </div>
     </div>
   );
 };
-const Ballet = ({ ballet }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+// BalletSection
+const BalletCard = ({ title, imageUrl, description, published }) => (
+  <div
+    style={{ backgroundColor: "#1E1F22" }}
+    className=" p-5 my-5 md:flex rounded-[16px] cursor-pointer hover:opacity-70 overflow-hidden"
+  >
+    <img
+      alt={title}
+      src={imageUrl}
+      className="h-[219.09px] w-[311px] md:h-[251px] md:w-[251px]"
+    />
+    <div className="text-white  flex flex-col mt-5 md:ml-[16px]">
+      <span className="md:h-[5.063rem] md:w-[16.938rem] md:text-[1.25rem] font-medium">
+        {title}
+      </span>
+      <span
+        style={{ color: "#FFFFFF99" }}
+        className="text-[14px] font-normal mt-[8px]"
+      >
+        {description}
+      </span>
+      <div className="text-[14px] font-normal flex flex-col mt-[52px]">
+        <span className="">Publish:</span>
+        <span className="">{published}</span>
+      </div>
+    </div>
+  </div>
+);
 
-  
-  const prevSlide = () => {
-    if(currentIndex === 0){
-      setCurrentIndex(ballet.length -1)
-    }else{
-      setCurrentIndex(currentIndex -1)
-    }
-  };
-
-  const nextSlide = () => {
-      if(currentIndex === ballet.length -1){
-        setCurrentIndex(0)
-      }
-      else setCurrentIndex(currentIndex  + 1)
-  }
-  console.log('test', currentIndex)
+const BalletComponent = ({ ballet, loadArticleHeader }) => {
   return (
-    <div className="mt-5 overflow-x-hidden">
+    <div className="mt-5">
       {!mobileSize && (
         <div className="flex flex-row items-center justify-between">
-          <h2 className="text-white text-[24px] font-bold">Broaway</h2>
-          <div className="flex gap-2">
-            <button onClick={prevSlide} >
-              <img className="h-[48px] w-[48px]" src={LeftArrow} alt="Previous" />
-            </button>
-            <button
-              onClick={nextSlide}
-        
-            >
-              <img className="h-[48px] w-[48px]" src={RightArrow} alt="Next" />
-            </button>
-          </div>
+          {loadArticleHeader && (
+            <>
+              <h2 className="text-white text-[24px] font-bold">Ballet</h2>
+              <div className="flex gap-2">
+                <button>
+                  <img className="h-[48px] w-[48px]" src={LeftArrow} alt="" />
+                </button>
+                <button>
+                  <img className="h-[48px] w-[48px]" src={RightArrow} alt="" />
+                </button>
+              </div>
+            </>
+          )}
         </div>
       )}
-      <div className="relative md:w-[1192px] overflow-hidden md:w-screen">
-        <div
-          className="mt-2 flex flex-col md:flex-row mt-5 gap-x-5"
-          style={{
-            transform: `translateX(-${currentIndex * 100 }%)`,
-            transition: "transform 0.5s ease",
-          }}
-        >
-          {ballet.map((ballet) => (
-            <div
-              key={ballet.id}
-              style={{ backgroundColor: "#1E1F22" }}
-              className="p-5 my-5 md:flex rounded-[16px] cursor-pointer hover:opacity-70 md:w-[870px] flex-shrink-0"
-            >
-              <img
-                alt={ballet.title}
-                src={ballet.imageUrl}
-                className="h-[219.09px] w-[311px] md:h-[251px] md:w-[251px]"
-              />
-              <div className="text-white flex flex-col mt-5 md:ml-[16px]">
-                <span className="md:h-[5.063rem] md:w-[16.938rem] md:text-[1.25rem] font-medium">
-                  {ballet.title}
-                </span>
-                <span style={{ color: "#FFFFFF99" }} className="text-[14px] font-normal mt-[8px]">
-                  {ballet.description}
-                </span>
-                <div className="text-[14px] font-normal flex flex-col mt-[52px]">
-                  <span>Publish:</span>
-                  <span>{ballet.published}</span>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
+      <div className="mt-2 flex flex-col md:flex-row  overflow-x-hidden mt-5 md:gap-x-5 ">
+        {ballet.map((ballet) => (
+          <BalletCard key={ballet.id} {...ballet} />
+        ))}
       </div>
     </div>
   );
 };
-const ArticleSelection = ({handleCategory}) => (
-   <div className="pt-5">
-          <div className="flex justify-between items-center">
-            <h2 className="text-white text-[24px] md:text-[32px] font-bold ">
-              Articles
-            </h2>
-            <select
-              className="text-white bg-slate-900 w-[143px] p-[16px] border-0 h-[56px] md:w-[180px] "
-              id="cars"
-              onChange={handleCategory}
-              style={{ borderRadius: "16px" }}
-            >
-              <option className="text-white" value="all">
-                All
-              </option>
-              <option className="text-white" value="broadway">
-                broadway
-              </option>
-              <option className="text-white" value="musical">
-                musical
-              </option>
-              <option className="text-white" value="ballet">
-                ballets
-              </option>
-            </select>
-          </div>
-        </div>
-)
+// Article Section
+const ArticleSelection = ({ handleCategory }) => {
+  return (
+    <div className="py-10">
+      <div className="flex justify-between items-center">
+        <h2 className="text-white text-[24px] md:text-[32px] font-bold ">
+          Articles
+        </h2>
+        <select
+          className="text-white bg-slate-900 w-[143px] p-[16px] border-0 h-[56px] md:w-[180px] focus:outline-none p-6"
+          id="cars"
+          onChange={handleCategory}
+          style={{ borderRadius: "16px" }}
+        >
+          <option className="text-white" value="all">
+            All
+          </option>
+          <option className="text-white" value="broadway">
+            broadway
+          </option>
+          <option className="text-white" value="musical">
+            musical
+          </option>
+          <option className="text-white" value="ballet">
+            ballets
+          </option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
 const ArticleComponent = () => {
   const [filter, setFilter] = useState("all");
   const [broadway, setBroadWay] = useState([]);
   const [musical, setMusical] = useState([]);
   const [ballet, setBallet] = useState([]);
-
+  const [loadArticle, setLoadArticle] = useState(false);
   const ListArticleData = async () => {
     const data = await getEvents();
-
-    setBroadWay(data.filter((broadway) => broadway.category === "broadway"));
-    setMusical(data.filter((musical) => musical.category === "musical"));
-    setBallet(data.filter((ballet) => ballet.category === "ballet"));
+    try {
+      if (data) {
+        setLoadArticle(true);
+        setBroadWay(
+          data.filter((broadway) => broadway.category === "broadway")
+        );
+        setMusical(data.filter((musical) => musical.category === "musical"));
+        setBallet(data.filter((ballet) => ballet.category === "ballet"));
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   useEffect(() => {
@@ -270,20 +242,26 @@ const ArticleComponent = () => {
   const handleCategory = (e) => {
     setFilter(e.target.value);
   };
-
   return (
     <div style={{ backgroundColor: "#000000" }}>
       <div className=" px-5 md:px-20 ">
-        <ArticleSelection handleCategory={handleCategory}/>
+        <ArticleSelection handleCategory={handleCategory} />
         <div className="flex flex-col">
           {(filter === "all" || filter === "broadway") && (
-            <Broadway broadway={broadway} />
+            <BroadwayComponent
+              broadway={broadway}
+              loadArticleHeader={loadArticle}
+              setFilter={setFilter}
+            />
           )}
           {(filter === "all" || filter === "musical") && (
-            <Musical musical={musical} />
+            <MusicalComponent
+              musical={musical}
+              loadArticleHeader={loadArticle}
+            />
           )}
           {(filter === "all" || filter === "ballet") && (
-            <Ballet ballet={ballet} />
+            <BalletComponent ballet={ballet} loadArticleHeader={loadArticle} />
           )}
         </div>
       </div>

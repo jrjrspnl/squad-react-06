d88dc1200c91e5dfc04ca09537867bfc18de6f6e
NicholaiNora <nicholai.nora.dict@gmail.com>
2025-06-08 09:57:52 +0800

    Update RelatedArticlesCard.jsx

--- Four-end-coders/src/components/NewsPage/RelatedArticlesCard.jsx
+++ Four-end-coders/src/components/NewsPage/RelatedArticlesCard.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 export default function RelatedArticleCard({ article }) {
   return (
     <Link to={`/article/${article.id}`}>
-      <div className="bg-[#1E1F22] p-3 rounded-lg overflow-hidden shadow-lg flex sm:flex-row flex-col gap-3 justify-center hover:bg-[#2a2b30] transition-colors">
+      <div className="bg-[#1E1F22] p-3 rounded-lg overflow-hidden shadow-lg flex sm:flex-row flex-col gap-3 justify-between hover:bg-[#2a2b30] transition-colors">
         <img
           src={article.imageUrl}
           alt={article.title}
-          className="w-[311px] h-[237px] sm:w-33 sm:h-fit object-cover rounded"
+          className="w-[311px] h-[237px] sm:w-33 sm:h-35 object-cover rounded"
         />
         <div className="w-full sm:w-52 flex flex-col gap-2">
           <h4 className="text-white font-sans text-sm font-semibold leading-normal">
